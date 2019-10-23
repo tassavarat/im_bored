5a0f7f82926008e275fcfdb173c4fe4a6b39d5c2
Sofia Cheung <sofiavictoria17@gmail.com>
2019-10-23 07:03:08 +0000

    Refactoring code, Adding positioned snake head and food in grid

--- src/snake/snake.js
+++ src/snake/snake.js
@@ -1,78 +1,18 @@
-import React, { useReducer, useState, useRef, useEffect } from 'react';
+import React from 'react';
 import './snake.css';
-const random = require('./random');
 
-function useInterval (callback, delay) {
-  const savedCallback = useRef();
+function random (min, max, array) {
+  min = Math.ceil(min) + 1;
+  max = Math.floor(max);
 
-  useEffect(() => {
-    savedCallback.current = callback;
-  });
-
-  useEffect(() => {
-    function tick () {
-      savedCallback.current();
-    }
-    const id = setInterval(tick, delay);
-    return () => clearInterval(id);
-  }, [delay]);
-}
-
-function Timer ({ pause }) {
-  const [hour, setHours] = useState(0);
-  const [minute, setMinutes] = useState(0);
-  const [second, setSeconds] = useState(0);
-
-  const toTime = (time) => ('0' + time).slice(-2);
-  const resetRef = useRef();
-  // Trick to Intialize countRef.current on first render only.
-  resetRef.current = resetRef.current || false;
-  useEffect(() => {
-    if (resetRef.current === true) {
-      setSeconds(0);
-    }
-  });
-
-  useInterval(() => {
-    if (pause) {
-      resetRef.current = true;
-      return;
-    }
-    resetRef.current = false;
-    setSeconds(second + 1);
-  }, pause ? null : 1000);
-
-  useInterval(() => {
-    if (pause) {
-      resetRef.current = true;
-      return;
-    }
-    resetRef.current = false;
-    setSeconds(0);
-    setMinutes(minute + 1);
-  }, pause ? null : 1000 * 60);
-
-  useInterval(() => {
-    if (pause) {
-      resetRef.current = true;
-      return;
-    }
-    setSeconds(0);
-    setMinutes(0);
-    setHours(hour + 1);
-  }, pause ? null : 1000 * 60 * 60);
-
-  return (
-    <div className='timer'>
-      <span>TIME:</span> <span>{toTime(hour)}:</span>
-      <span>{toTime(minute)}:</span>
-      <span>{toTime(second)}</span>
-    </div>
-  );
+  while (true) {
+    const num = Math.floor(Math.random() * (max - min)) + min;
+    if (!array) return num;
+    if (!array.includes(num)) return num;
+  }
 }
 
 function initGrid (size) {
-  // const grid = Array.from(Array(size), () => Array(size).fill('x'));
   const grid = [];
   for (let row = 0; row < size; ++row) {
     const cols = [];
@@ -93,17 +33,6 @@ function initState () {
   const max = size - 1;
   const grid = initGrid(size);
 
-  /*
-  grid[min].fill('x');
-  for (let i = 1; i < size; ++i) {
-    grid[i][min] = 'x';
-    grid[i][max] = 'x';
-  }
-  grid[max].fill('x');
-  grid[max / 2][max / 2] = '#';
-  grid[random(min, max)][random(min, max)] = '*';
-  */
-
   return {
     grid,
     snake: {
@@ -116,163 +45,42 @@ function initState () {
     food: {
       row: random(min, max),
       col: random(min, max)
-    },
-    showGrid: true,
-    inprogress: false
+    }
   };
 }
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'game_lost':
-      return {
-        ...state,
-        showGrid: state.showGrid,
-        lost: true,
-        message: 'Press <space> or touch/click to start the game',
-        inprogress: false
-      };
-    case 'update':
-      // console.log('update state', action.newState);
-      return {
-        ...state,
-        ...action.newstate
-      };
-    case 'toggle_grid':
-      return {
-        ...state,
-        showGrid: !state.showGrid
-      };
-    case 'restart':
-      const newState = {
-        ...state,
-        message: 'Game in progress ☝',
-        inprogress: true,
-        lost: false,
-        snake: {
-          ...state.snake,
-          head: {
-            row: Math.floor(random() * 5),
-            col: Math.floor(random() * 5)
-          },
-          tail: []
-        }
-      };
-      return newState;
-    default: {
-      return state;
-    }
-  }
-};
-
-const Keys = {
-  Space: 32,
-  Left: 37,
-  Up: 38,
-  Right: 39,
-  Down: 40,
-  a: 65,
-  w: 87,
-  s: 83,
-  d: 68
-};
-
-var move = Keys.Up;
-
-function Display () {
-  const [state, dispatch] = useReducer(reducer, initState());
-  let interval = null;
-
+function displayGrid () {
+  const state = initState();
   const cellStyle = (cell) => {
-    const { snake, food, showGrid } = state;
-    let style = 'cell ';
-    if (snake.head.row === cell.row && snake.head.col === cell.col) {
-      style = 'cell head head-up';
-      if (move === Keys.Left) {
-        style = 'cell head head-left';
-      } else if (move === Keys.Up) {
-        style = 'cell head head-up';
-      } else if (move === Keys.Right) {
-        style = 'cell head head-right';
-      } else if (move === Keys.Down) {
-        style = 'cell head head-down';
-      }
-    } else if (food.row === cell.row && food.col === cell.col) {
-      style = 'cell food';
-    } else if (snake.tail.find(t => t.row === cell.row && t.col === cell.col)) {
-      style = 'cell tail';
+    let style = 'cell';
+    if ((cell.row === state.food.row) && (cell.col === state.food.col)) {
+      style = 'food';
+    } else if ((cell.row === state.snake.head.row) &&
+      (cell.col === state.snake.head.col)) {
+      style = 'snakeHead';
     }
-
-    style = showGrid ? style + ' cell-border' : style;
     return style;
   };
+  return (
+    state.grid.map((row) => {
+      return row.map((cell) => {
+        const st = cellStyle(cell);
+        return (
+          <div
+            className={st}
+            key={row + cell.col}
+          />
+        );
+      });
+    })
+  );
+}
 
-  const displayGrid = () => {
-    const { grid } = state;
-    return (
-      grid.map((row, i) => {
-        return row.map(cell => {
-          let actorStyle = cellStyle(cell);
-          return (
-            <div
-              key={cell.row + cell.col}
-              className={actorStyle}
-            />
-          );
-        });
-      })
-    );
-  };
-
-  useEffect(() => {
-    GameLoop();
-  }, []);
-
-  const useTimeout = (fctn, time) => {
-    interval = setTimeout(fctn, time);
-  };
-
-  const GameLoop = () => {
-    let moveX = 0;
-    let moveY = -1;
-
-    if (move === Keys.Left) {
-      moveX = -1;
-      moveY = 0;
-    } else if (move === Keys.Right) {
-      moveX = 1;
-      moveY = 0;
-    } else if (move === Keys.Up) {
-      moveX = 0;
-      moveY = -1;
-    } else if (move === Keys.Down) {
-      moveX = 0;
-      moveY = 1;
-    }
-
-    const nextState = {
-      snake: {
-        ...state.snake,
-        head: {
-          row: state.snake.head.row + moveY,
-          col: state.snake.head.col + moveX
-        }
-      },
-      food: 1
-    };
-
-    dispatch({
-      type: 'update',
-      newState: nextState
-    });
-
-    clearTimeout(interval);
-    useTimeout(GameLoop, 1000 / 6);
-  };
-
+function Display () {
   return (
     <div className='game'>
       <div className='wrapper'>
+        <h1> Snake </h1>
         <div className='grid'>
           {displayGrid()}
         </div>
