da9a7b8e5bedd77a5aba9c24271eb383e9e29ab8
Tim Assavarat <tassavarat@gmail.com>
2019-10-24 21:02:32 -0700

    Switch from useState to useReducer

--- src/snake/snake.js
+++ src/snake/snake.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import './snake.css';
 
 /**
@@ -68,20 +68,54 @@ function InitState () {
   };
 }
 
+function reducer (state, action) {
+  /*
+    left: { row: state.snake.head.row, col: state.snake.head.col - 1 },
+    up: { row: state.snake.head.row - 1, col: state.snake.head.col },
+    right: { row: state.snake.head.row, col: state.snake.head.col + 1 },
+    down: { row: state.snake.head.row + 1, col: state.snake.head.col }
+  };
+  */
+  switch (action.type) {
+    case 'ArrowUp':
+      return { ...state, snake: { head: { row: state.snake.head.row - 1, col: state.snake.head.col } } };
+    default:
+      throw new Error();
+  }
+}
+
+// state => ({ ...state, snake: { head: direction.up } })
+const mapper = {
+  37: 'ArrowRight',
+  38: 'ArrowUp',
+  39: 'ArrowRight',
+  40: 'ArrowDown'
+};
+
 /**
  * displayGrid - Changes cell's className according to position in cell
  *
  * Return: Div tag containing correct className for each cell
  */
 function DisplayGrid () {
-  const [state, setState] = useState(InitState());
+  const [state, dispatch] = useReducer(reducer, InitState());
+  console.log('stuff', state);
 
-  const direction = {
-    left: { row: state.snake.head.row, col: state.snake.head.col - 1 },
-    up: { row: state.snake.head.row - 1, col: state.snake.head.col },
-    right: { row: state.snake.head.row, col: state.snake.head.col + 1 },
-    down: { row: state.snake.head.row + 1, col: state.snake.head.col }
+  const newDirection = e => {
+    if (mapper[e.keyCode]) {
+      dispatch(mapper[e.keyCode]);
+    }
   };
+
+  useEffect(() => {
+    window.addEventListener('keydown', newDirection);
+    const onTick = () => {
+      // console.log('onTick');
+    };
+
+    const interval = setInterval(onTick, 250);
+    return () => clearInterval(interval);
+  }, [state]);
 
   const cellStyle = (cell) => {
     let style = 'cell';
@@ -93,50 +127,6 @@ function DisplayGrid () {
     }
     return style;
   };
-
-  useEffect(() => {
-    const onTick = () => {
-      window.addEventListener('keydown', (e) => {
-        switch (e.key) {
-          case 'Down': // IE/Edge specific value
-          case 'ArrowDown':
-            setState(state => ({ ...state, snake: { head: direction.down } }));
-            // Do something for 'down arrow' key press.
-            break;
-          case 'Up': // IE/Edge specific value
-          case 'ArrowUp':
-            setState(state => ({ ...state, snake: { head: direction.up } }));
-            // Do something for 'up arrow' key press.
-            break;
-          case 'Left': // IE/Edge specific value
-          case 'ArrowLeft':
-            setState(state => ({ ...state, snake: { head: direction.left } }));
-            // Do something for 'left arrow' key press.
-            break;
-          case 'Right': // IE/Edge specific value
-          case 'ArrowRight':
-            setState(state => ({ ...state, snake: { head: direction.right } }));
-            // Do something for 'right arrow' key press.
-            break;
-          default:
-        }
-      }, true);
-      /*
-        if (e.key === 'ArrowUp') {
-        } else if (e.key === 'ArrowDown') {
-          key = { row: state.snake.head.row + 1, col: state.snake.head.col };
-        } else if (e.key === 'ArrowRight') {
-          key = { row: state.snake.head.row, col: state.snake.head.col + 1 };
-        } else if (e.key === 'ArrowLeft') {
-          key = { row: state.snake.head.row, col: state.snake.head.col - 1 };
-        }
-      });
-      */
-    };
-
-    const interval = setInterval(onTick, 250);
-    return () => clearInterval(interval);
-  }, [state]);
 
   return (
     state.grid.map((row) => {
