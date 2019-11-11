56b4ff734d124d3f6211228d7715b9e5bcb55714
Sofia Cheung <sofiavictoria17@gmail.com>
2019-11-11 06:20:32 +0000

    Adding an about button in footer of page

--- src/home/main.js
+++ src/home/main.js
@@ -17,6 +17,7 @@ const Home = () => (
       <ul>
         <li><a href='https://www.linkedin.com/in/sof%C3%ADa-cheung-90056817a/'> Sofía Cheung </a></li>
         <li><a href='https://www.linkedin.com/in/tim-assavarat-04b14717a/'> Tim Assavarat </a></li>
+        <li className='aboutLink'><NavLink to='/about'>About</NavLink></li>
       </ul>
       <form action='https://github.com/tassavarat/im_bored'>
         <input className='glink' type='submit' value='view on Github' />
@@ -38,11 +39,26 @@ const snake = () => (
   </div>
 );
 
+const about = () => (
+  <div className='about'>
+    <div className='box'>
+      <h1> About </h1>
+      <p> I'm Bored was created to allow people to have easy access to
+          casual games and was inspired by existing online gaming
+          websites. The idea was accessibility and intuitiveness so that
+          anyone can understand and play the games.<br />
+          Made by Sofia Cheung and Tim Assavarat.
+      </p>
+    </div>
+  </div>
+);
+
 const Main = () => (
   <Switch>
-    <Route exact path='/' component={Home}></Route>
-    <Route exact path='/ticTacToe' component={ticTacToe}></Route>
-    <Route exact path='/snake' component={snake}></Route>
+    <Route exact path='/' component={Home} />
+    <Route exact path='/ticTacToe' component={ticTacToe} />
+    <Route exact path='/snake' component={snake} />
+    <Route exact path='/about' component={about} />
   </Switch>
 );
 
