fc6a72b08493dc06de694a969a8a8ecc1a126dab
Sofia Cheung <sofiavictoria17@gmail.com>
2020-05-08 01:34:24 -0700

    Making homepage responsive, re-organizing css styling, Adding svg social icons

--- src/home/App.js
+++ src/home/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Main from './main.js';
-import { NavLink } from 'react-router-dom';
 
 const App = () => (
   <div className='app'>
-    <nav className='title'>
-      <h1><NavLink to='/'> Im Bored...</NavLink></h1>
-      <div className='instruction'> choose a game to play </div>
-    </nav>
+    <section className='title-section'>
+      <h1><a href='/' className='title-link'> Im Bored... </a><small className='instruction'> choose a game to play </small></h1>
+    </section>
     <Main />
   </div>
 );
