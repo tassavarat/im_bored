7a6c63d26bf56e8b401b1e1e13dae0c9ef1c2b6d
Sofia Cheung <sofiavictoria17@gmail.com>
2023-02-02 17:59:28 -0800

    Add create board logic

--- src/memory/Memory.jsx
+++ src/memory/Memory.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useReducer } from 'react'
+import React, { useState, useReducer, useEffect } from 'react'
+import './Memory.css'
 
 /**
  * 
@@ -9,36 +10,86 @@ import React, { useState, useReducer } from 'react'
 const getRandomNum = (min, max) => {
   min = Math.ceil(min)
   max = Math.floor(max)
-  return Math.ceil(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 /**
  * [[., ., ., ., *]
  *  [*, *, ., ., *]
- *  [., ., ., *, .]
- * ]
+ *  [., ., ., *, .]]
+ * @returns board with empty and highlighted cells
  */
 const createBoard = () => {
-  const board = Array.from(Array(6), _ => Array(6).fill('.'))
-  const numOfHighlightedCells =  getRandomNum(0, 6)
-  console.log('num', numOfHighlightedCells)
+  const board = Array.from(Array(4), _ => Array(4).fill('.'))
+  const numOfHighlightedCells =  6
+  for (let i = 0; i <= numOfHighlightedCells; i++) {
+    const randRow = getRandomNum(0, 3)
+    const randCol = getRandomNum(0, 3)
+
+    board[randRow][randCol] = '*'
+  }
+  return board
+}
+
+/**
+ * updates the board with user input
+ */
+const updateBoard = () => {
+
 }
 
 // createBoard -> create board with highlighted cells
 // checkBoard -> check if selected cells are equal to the highlighted cells
 const reducer = (grid, action) => {
-  switch (action.type) {
+  switch (action) {
     case 'createBoard': 
       return createBoard()
+    case 'updateBoard':
+      return updateBoard()
   }
 }
 
 const Memory = () => {
-  const [grid, dispatch] = useReducer(reducer, createBoard())
+  const [grid, dispatch] = useReducer(reducer, [])
+  const [showHighlightedCells, setShowHighlightedCells] = useState(false)
+
+  /**
+   * create the board and display highlighted cells to memorize on a timer.
+   */
+  const startGame = () => {
+    dispatch('createBoard')
+    setShowHighlightedCells(true)
+    setTimeout(() => {
+      setShowHighlightedCells(false)
+    }, 4000)
+  }
+
+  const cellOnClick = (row, col) => {
+
+  }
 
   return (
-    <div>Memory Game</div>
-  )
+    <div>
+      <div className='m-board-wrapper'>
+        <h2>Memory Game</h2>
+        {!grid?.length ? (
+          <div className='start'>
+            <button onClick={startGame}>Start</button>
+          </div>
+        ) : (
+          <div className='m-board'> 
+            {grid && grid.map((_, rowx) => (
+              <div className='row' key={`row-${rowx}`}>
+                {grid[rowx] && grid[rowx].map((col, colIdx) => (
+                  <div className={`m-cell ${showHighlightedCells && col === '*' ? 'highlighted' : 'm-cell'}`} key={`col-${colIdx}`}>{col}</div>
+                ))}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+    )
 }
 
 export default Memory
